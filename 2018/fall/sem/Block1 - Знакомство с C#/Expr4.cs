ab1ba97d62d672de493ddee1b7e4a643c74badf0
SharafeevRavil <graf.rav@gmail.com>
2018-09-12 13:20:07 +0300

    Update Expr4.cs
    [FIX] merge conflict

--- 2018/fall/sem/Block1 - Знакомство с C#/Expr4.cs
+++ 2018/fall/sem/Block1 - Знакомство с C#/Expr4.cs
@@ -10,20 +10,12 @@ namespace Expr4 {
 			int n = Int32.Parse(Console.ReadLine());
 			int x = Int32.Parse(Console.ReadLine());
 			int y = Int32.Parse(Console.ReadLine());
-
-<<<<<<< HEAD:2018/fall/sem/Block1 - Знакомство с C#/Expr4.cs
-			Console.WriteLine( (int)(((double)n-0.5)/x) + (int)(((double)n - 0.5)/y) - (int)(((double)n - 0.5)/(SomeFunc(x, y)) );
+			
+			Console.WriteLine((int)(((double)n - 0.5) / x) + (int)(((double)n - 0.5) / y) - (int)(((double)n - 0.5) / (SomeFunc(x, y))));
 			Console.ReadKey();
 		}
-
+		
 		static int SomeFunc(int x, int y) {
-=======
-			Console.WriteLine((int)(((double)n - 0.5) / x) + (int)(((double)n - 0.5) / y) - (int)(((double)n - 0.5) / (Some(x, y))));
-			Console.ReadKey();
-		}
-
-		static int Some(int x, int y) {
->>>>>>> 8d07a7b56ee5d429c3675e8fb9dfdd245bc282cd:2018/fall/sem/Expr4.cs
 			if (x == y) {
 				return x;
 			}
