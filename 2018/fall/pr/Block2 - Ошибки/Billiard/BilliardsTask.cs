ea29e4ab0a3f4bd4c272603e3f2d57a50b2f1380
SharafeevRavil <graf.rav@gmail.com>
2018-09-17 13:13:35 +0300

    Update BilliardsTask.cs

--- 2018/fall/pr/Block2 - Ошибки/Billiard/BilliardsTask.cs
+++ 2018/fall/pr/Block2 - Ошибки/Billiard/BilliardsTask.cs
@@ -1,3 +1,9 @@
+/*
+ * Решается задача: https://ulearn.me/Course/BasicProgramming/Praktika_Bil_yard__e191b760-98cd-4d54-981a-971f0b4e319d?version=-1
+ * 
+ * Автор решения: Шарафеев Равиль, 11-808
+ */
+
 ﻿using System;
 
 namespace Billiards
