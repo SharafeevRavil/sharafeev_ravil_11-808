51ac6ddab33b46ed6d93b7bf4aa73059957f0924
Ravil <graf.rav@gmail.com>
2020-04-12 15:10:16 +0300

    [done] exercise 4

---
+++ 2020/Spring/SocialNetwork/SocialNetworkMVC/AOP/AuthenticatedAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SocialNetworkMVC.Models;
+
+namespace SocialNetworkMVC.AOP
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class AuthenticatedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var dbContext = context.HttpContext.RequestServices.GetService<ApplicationDbContext>();
+            var userIdStr = context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userIdStr))
+            {
+                context.Result = new RedirectToActionResult("AccessDenied", "Account", null);
+            }
+            else
+            {
+                var user = dbContext.Users.Find(int.Parse(userIdStr));
+                if (user == null)
+                {
+                    context.Result = new RedirectToActionResult("AccessDenied", "Account", null);
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
